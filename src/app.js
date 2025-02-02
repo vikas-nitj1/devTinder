b84d08c20186982d5cc2a0834fd44f1264881f08
vikas-nitj1 <vikasknv123@gmail.com>
2025-02-02 11:25:55 +0530

    Playing with the routes

--- src/app.js
+++ src/app.js
@@ -2,8 +2,24 @@ const express = require('express');
 
 const app = express();
 
-app.use('/',(req,res)=>{
-    res.send("Hello World");
+
+app.use('/user',(req,res)=>{
+    res.send("HAHAAHAHAHAHA")
+})
+
+app.get('/user',(req,res)=>{
+    res.send({"Name":"Vikas Kumaer",
+        "Age":25,
+        "Occupation":"Software Engineer"
+    })
+})
+
+app.post('/user',(req,res)=>{
+    res.send("Data Sucessfully loaded to the Database!");
+})
+
+app.delete('/user',(req,res)=>{
+    res.send('Data succesfully deleted from the Database');
 })
 
 app.listen(3000,()=>{
